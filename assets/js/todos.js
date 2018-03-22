6227ff6d93f56a01d5d459c57f51a7019c401919
Hector Perea <hperea.alfa@gmail.com>
2018-03-22 16:34:31 -0500

    finishing the basic functionality of the todo app

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,3 +1,23 @@
-$("li").click(function() {
+//Toggle the css class completed in all the li's
+$("ul").on("click", "li", function(e) {
     $(this).toggleClass("completed-js");
+});
+
+//fade out and remove the lis when click in the X
+$("ul").on("click", "span", function(e){
+	$(this).parent().fadeOut(500, function() {
+		$(this).remove();
+	});
+	e.stopPropagation();
+});
+
+//adding the functionality to the input
+$("input[type='text'").keypress(function(e){
+	if(e.which === 13) {
+		//extracting the text from the input
+		var textInput = $(this).val();
+		$(this).val("");
+		//append the text from the input to a new li inside the ul
+		$("ul").append("<li><span>X</span> " + textInput + "</li>");
+	}
 });
